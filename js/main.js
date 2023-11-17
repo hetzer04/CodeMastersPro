05b9b6e524ddd79fbb0cba12822de24774f314e9
Евгений Сагайдак <sagaydack@gmail.com>
2023-11-17 22:57:23 +0600

    burger+

--- js/main.js
+++ js/main.js
@@ -69,3 +69,13 @@ window.addEventListener("keydown", function (event) {
     modalCallForm.classList.remove("modal-parent--open")
   }
 })
+
+// Бургер
+const burger = document.getElementById("burger")
+const nav = document.getElementById("nav")
+
+burger.addEventListener("click", function () {
+  nav.classList.toggle("navi--active")
+  burger.classList.toggle("burger--active")
+  document.body.classList.toggle("stop-scroll")
+})
