675230bb2d1a55bcf01a691cac5f0ac6946472a5
Евгений Сагайдак <sagaydack@gmail.com>
2023-11-17 15:05:46 +0600

    17.11.2023

--- js/main.js
+++ js/main.js
@@ -45,3 +45,27 @@ btnDarkMode.onclick = function () {
 };
 
 // modal
+// Модальное окно
+const callFromBtn = document.getElementById("call-form")
+const modalCallForm = document.getElementById("modal-call-form")
+
+// Открытие модального окна
+callFromBtn.addEventListener("click", function () {
+  modalCallForm.classList.add("modal-parent--open")
+})
+
+// Закрытие модального окна
+modalCallForm.querySelector(".modal").addEventListener("click", function (event) {
+  event._isClick = true
+})
+modalCallForm.addEventListener("click", function (event) {
+  if (event._isClick === true) return
+  modalCallForm.classList.remove("modal-parent--open")
+})
+
+// Закрытие при нажатии на Esc
+window.addEventListener("keydown", function (event) {
+  if (event.key === "Escape") {
+    modalCallForm.classList.remove("modal-parent--open")
+  }
+})
